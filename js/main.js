5f3c5de5f73e9246701d7ea1638f621f19a547b8
mat-kozielski <mat.kozielski@gmail.com>
2020-07-30 20:38:52 +0200

    Refactoring_styles_and_structure

--- js/main.js
+++ js/main.js
@@ -1,117 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector(".calc__form");
-    const summary = document.querySelector(".calc__summary");
-
-    new Calc(form, summary);
-});
-
-function Calc(form, summary) {
-
-    //Price var object
+function Calculator(form, summary) {
     this.prices = {
         products: 0.5,
         orders: 0.25,
         package: {
             basic: 0,
             professional: 25,
-            premium: 60,
+            premium: 60
         },
-        accounting: 40,
-        terminal: 10
+        accounting: 35,
+        terminal: 5
     };
 
-    //Form elements
+    /**
+     * Inputs / Select / Checkbox
+     */
     this.form = {
-        products: form.querySelector('#products'),
-        orders: form.querySelector('#orders'),
-        package: form.querySelector('#package'),
-        accounting: form.querySelector('#accounting'),
-        terminal: form.querySelector('#teminal'),
-    }
+        products: form.querySelector("#products"),
+        orders: form.querySelector("#orders"),
+        package: form.querySelector("#package"),
+        accounting: form.querySelector("#accounting"),
+        terminal: form.querySelector("#terminal")
+    };
 
-    //Summary elements
+    /**
+     * Summary elements
+     */
     this.summary = {
-        list: summary.querySelector('ul'),
-        items: summary.querySelector('ul').children,
+        list: summary.querySelector("ul"),
+        items: summary.querySelector("ul").children,
         total: {
-            container: summary.querySelector('#total-price'),
-            price: summary.querySelector('.total-price')
+            container: summary.querySelector("#total-price"),
+            price: summary.querySelector(".total__price")
         }
     };
+
+
+    // Init
+    this.addEvents();
 }
 
-
-//InputEvent summary method - counting prices
-Calc.prototype.inputEvent = function (element) {
-    const id = element.currentTarget.id;
-    const value = element.currentTarget.value;
-    const singlePrice = this.prices[id];
-    const totalPrice = value * singlePrice;
-
-    //Starting updateSummary method
-    this.updateSummary(id, value + ' * $' + singlePrice, totalPrice, function (item, calc, total) {
-        if (value < 0) {
-            calc.innerHTML = null;
-            total.innerText = 'Value should be greater than 0';
-        }
-        if (value.length === 0) {
-            item.classList.remove('open');
-        }
-    })
-}
-
-//UpdateSummary summary method - updating data in summary section
-Calc.prototype.updateSummary = function (id, calc, total, callback) {
-    const summary = this.summary.list.querySelector('[data-id=" + id + "]');
-    const summaryCalc = summary.querySelector('.item__calc');
-    const summaryTotal = summary.querySelector('.item__price');
-
-    //Adding 'open' class
-    summary.classList.add('open');
-
-    if (summaryCalc !== null) {
-        summaryCalc.innerText = calc;
-    }
-
-    summaryTotal.innterText = "$" + total;
-
-    if (typeof callback === "function") {
-        callback(summary, summaryCalc, summaryTotal);
-    }
-};
-
-//Toggle event on select
-Calc.prototype.selectEvent = function (element) {
-    this.form.package.classList.toggle('open');
-
-    const value = typeof element.target.dataset.value !== 'undefined' ? element.target.dataset.value : '';
-    const text = typeof element.target.dataset.value !== 'undefined' ? element.target.innerText : 'Choose package';
-
-    //Actualization condition
-    if (value.length > 0) {
-        this.form.package.dataset.value = value;
-        this.form.package.querySelector('.select__input').innerText = text;
-
-        this.updateSummary('package', text, this.prices.package[value]);
-    }
-};
-
-//Checkbox events
-Calc.prototype.checkboxEvent = function (element) {
-    const checkbox = element.currentTarget;
-    const id = checkbox.id;
-    const checked = element.currentTarget.checked;
-
-    this.updateSummary(id, undefined, this.prices[id], function (item) {
-        if (!checked) {
-            item.classList.remove("open");
-        }
-    });
-};
-
-//AddEvents method on all elements
-Calc.prototype.addEvents = function () {
-    //Inputs
+Calculator.prototype.addEvents = function () {
+    // Inputs
     this.form.products.addEventListener("change", this.inputEvent.bind(this));
     this.form.products.addEventListener("keyup", this.inputEvent.bind(this));
     this.form.orders.addEventListener("change", this.inputEvent.bind(this));
@@ -125,9 +54,7 @@ Calc.prototype.addEvents = function () {
     this.form.terminal.addEventListener("change", this.checkboxEvent.bind(this));
 };
 
-
-//AddEvents on summing funtion
-Calc.prototype.updateTotal = function () {
+Calculator.prototype.updateTotal = function () {
     const show = this.summary.list.querySelectorAll(".open").length > 0;
 
     if (show) {
@@ -145,8 +72,78 @@ Calc.prototype.updateTotal = function () {
     }
 };
 
+Calculator.prototype.updateSummary = function (id, calc, total, callback) {
+    const summary = this.summary.list.querySelector("[data-id=" + id + "]");
+    const summaryCalc = summary.querySelector(".item__calc");
+    const summaryTotal = summary.querySelector(".item__price");
+
+    summary.classList.add("open");
+
+    if (summaryCalc !== null) {
+        summaryCalc.innerText = calc;
+    }
+
+    summaryTotal.innerText = "$" + total;
+
+    if (typeof callback === "function") {
+        callback(summary, summaryCalc, summaryTotal);
+    }
+};
+
+Calculator.prototype.inputEvent = function (e) {
+    const id = e.currentTarget.id;
+    const value = e.currentTarget.value;
+    const singlePrice = this.prices[id];
+    const totalPrice = value * singlePrice;
+
+    this.updateSummary(id, value + " * $" + singlePrice, totalPrice, function (item, calc, total) {
+        if (value < 0) {
+            calc.innerHTML = null;
+            total.innerText = "Value should be greater than 0";
+        }
+
+        if (value.length === 0) {
+            item.classList.remove("open");
+        }
+    });
+
+    this.updateTotal();
+};
 
 
+Calculator.prototype.selectEvent = function (e) {
+    this.form.package.classList.toggle("open");
+
+    const value = typeof e.target.dataset.value !== "undefined" ? e.target.dataset.value : "";
+    const text = typeof e.target.dataset.value !== "undefined" ? e.target.innerText : "Choose package";
+
+    if (value.length > 0) {
+        this.form.package.dataset.value = value;
+        this.form.package.querySelector(".select__input").innerText = text;
+
+        this.updateSummary("package", text, this.prices.package[value]);
+        this.updateTotal();
+    }
+};
+
+Calculator.prototype.checkboxEvent = function (e) {
+    const checkbox = e.currentTarget;
+    const id = checkbox.id;
+    const checked = e.currentTarget.checked;
+
+    this.updateSummary(id, undefined, this.prices[id], function (item) {
+        if (!checked) {
+            item.classList.remove("open");
+        }
+    });
+
+    this.updateTotal();
+};
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector(".calc__form");
+    const summary = document.querySelector(".calc__summary");
 
+    new Calculator(form, summary);
+});
